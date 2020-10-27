8d6534a9c498ef03c2001002a0f52cda39269c08
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2020-10-26 23:54:10 -0700

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The grades of the student are:" + "\nQuizze1 :" + _quizze1 + "n\Quizze2 : " + _quizze2 + "n\Quizze3:" + _quizze3 + "n\MidtermExam:" + _midtermExam + "\nFinalExam:" + _finalExam + " n\Grade:" + grade);
+            Console.ReadLine();
+         }
+    }
+}
---
+++ Project1/Student.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    class Student
+    {
+        private double _quizze1;
+        private double _quizze2;
+        private double _quizze3;
+        private double _midtermExam;
+        private double _finalExam;
+        private double grade;
+
+
+        public Student(double quizze1, double quizze2, double quizze3,
+                     double midtermExam, double finalExam)
+        {
+            this.MidtermExam = midtermExam;
+            this.FinalExam = finalExam;
+            this.Quizze1 = quizze1;
+            this.Quizze2 = quizze2;
+            this.Quizze3 = quizze3;
+        }
+
+        public double Quizze1
+        {
+            get { return _quizze1; }
+            private set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentOutOfRangeException("The grade is between 0 and 10.");
+                }
+                _quizze1 = value;
+            }
+        }
+        public double Quizze2
+        {
+            get
+            {
+                return _quizze2;
+            }
+            set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentOutOfRangeException("The grade is between 0 and 10.");
+                }
+                _quizze2 = value;
+            }
+        }
+
+        public double Quizze3
+        {
+            get { return _quizze3; }
+            private set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentOutOfRangeException("The grade is between 0 and 10.");
+                }
+                _quizze3 = value;
+            }
+        }
+        public double MidtermExam
+        {
+            get { return _midtermExam; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("The grade is between 0 and 100.");
+                }
+                _midtermExam = value;
+            }
+        }
+        public double FinalExam
+        {
+            get { return _finalExam; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("The grade is between 0 and 100.");
+                }
+                _finalExam = value;
+            }
+        }
+
+        public double FinalGrade()
+        {
+            double quizzesGrades = (Quizze1 * 0.25) +
+                                        (Quizze2 * 0.25) +
+                                        (Quizze3 * 0.25);
+
+            MidtermExam = (MidtermExam * 0.35);
+
+            FinalExam = (FinalExam * 0.4);
+
+            grade = quizzesGrades + MidtermExam + FinalExam;
+            return grade;
+        }
+    }
+}
+
+
+
+
+
+   
+
